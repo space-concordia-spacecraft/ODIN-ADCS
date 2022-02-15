ea5a94c066922ced815ab600423e94b8b3dbb397
Marioa95 <76663281+Marioa95@users.noreply.github.com>
2022-02-15 17:55:03 -0500

    Changes in test model

--- Initialization/Initialization.m
+++ Initialization/Initialization.m
@@ -1,12 +1,27 @@
 %% Mission Parameters
 
+<<<<<<< Updated upstream
+=======
+%Mission epoch
+Yr = 2000;
+Month = 01;
+Day = 01;
+Hr = 12;
+Min = 00;
+Sec = 00;
+JD = jday(Yr,Month,Day,Hr,Min,Sec);
+>>>>>>> Stashed changes
 
+%Initial TLE
 
 %% Spacecraft mass properties
 
 %Mass
 
 %Inertia
+J =[0.04582275 0 0;
+    0 0.04561065 0;
+    0 0 0.0085932349] ;
 
 %% Components orientation with respect to spacecraft
 
@@ -20,6 +35,8 @@
 
 %% Reaction Wheels characteristics
 
-% Wheel Inertia
+Jw = 2.029e-6; % Wheel Inertia
 
-Jw = 2.11e-6;
+f = 4.55e-6;% Viscous Damping
+
+%
